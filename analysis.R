3849e45eba61bd65403f49525ad1b9cd16641938
rondeline <rmw237@stanford.edu>
2024-04-04 21:38:38 -0700

    upload survey images

--- analysis.R
+++ analysis.R
@@ -1,11 +1,4 @@
----
-  title: "MetaNoise"
-author: "Rondeline M. Williams"
-date: '2024-03-05'
-output: html_document
----
-  
-  #Load libraries
+#Load libraries
 library(here)
 library(dplyr)
 library(tidyr)
@@ -13,103 +6,195 @@ library(janitor)
 library(qualtRics)
 library(stringr)
 library(data.table)
+library(ggplot2)
+library(rstanarm)
 
 #Load data
-metanoise_p1 <- read_survey("metanoise_p1.csv") 
+metanoise_p2 <- read_survey("metanoise_p2.csv") 
 
 #Set as df
-setDF(metanoise_p1)
+setDF(metanoise_p2)
 
-#Tidy data
-metanoise_p1_tidy <- metanoise_p1 |> 
-  clean_names() |> 
-  mutate(subject_id = row_number()) |> 
-  select(eligible_children:questions, -c(q547_1:q547_7)) |> 
-  rename(
-    "multi_household" = "q543",
-    "number_multi_household" = "q544",
-    "house_type" = "q545",
-    "other_house_type" = "q545_8_text",
-    "tv_hours" = "q546",
-    "commotion" = "q548",
-    "zoo" = "q549",
-    "no_interrupted" = "q550",
-    "fuss" = "q551",
-    "no_think" = "q552",
-    "relax" = "q553",
-    "calm" = "q554",
-    "bad_fitted_doors" = "q555_1",
-    "noise_between_rooms" = "q555_2",
-    "noise_other_homes" = "q555_3",
-    "noise_streets" = "q555_4",
-    "trash" = "q555_5",
-    "traffic" = "q556",
-    "stores_walking" = "q558",
-    "publictrans_walking" = "q559",
-    "traffic_walking" = "q560",
-    "traffic_biking" = "q561",
-    "overall_experience" = "q292",
-    "clarity" = "q293",
-    "errors" = "q294"
-  ) |> 
-  pivot_longer(cols = o1_babble_1:music_o4_13,
+#Tidy demographic data
+metanoise_p2_demographics <- metanoise_p2 |> 
+  clean_names() |>
+  select(response_id, age_1_4:age_3_4,sex_1_1:traffic_bike, age_caregiver:education_adult) |>
+  rename_with(~str_remove(.x, "_\\d+$"), everything()) |> 
+  pivot_longer(cols = age_1:diagnosis_3,
+               names_to = c(".value", "set"),
+               names_pattern = "(.*)_(\\d+)$") |> 
+  mutate(set = str_replace(set, "_\\d+$", ""),
+         set = as.numeric(set)) |>
+  filter(!is.na(age),
+         !is.na(sex))
+
+#Tidy trial data
+metanoise_p2_trials <- metanoise_p2 |> 
+  clean_names() |>
+  select(response_id, x1_babble_1:x3_construction_13) |>
+  pivot_longer(cols = -response_id,
+               names_to = c("set", ".value"),
+               names_sep = "(?<=x\\d)_") |> 
+  mutate(set = case_when(set == "x1" ~ 1,
+                         set == "x2" ~ 2,
+                         set == "x3" ~ 3)) |> 
+  pivot_longer(cols = -c(response_id, set),
                names_to = "sound",
                values_to = "rating") |> 
-  mutate(
-    activity = case_when(
-      str_detect(sound, "_1$") ~ "listen_story",
-      str_detect(sound, "_2$") ~ "sleep",
-      str_detect(sound, "_3$") ~ "learn_game",
-      str_detect(sound, "_4$") ~ "dance",
-      str_detect(sound, "_5$") ~ "math",
-      str_detect(sound, "_6$") ~ "talk_about_day",
-      str_detect(sound, "_7$") ~ "remember_list",
-      str_detect(sound, "_8$") ~ "brush_teeth",
-      str_detect(sound, "_9$") ~ "puzzle",
-      str_detect(sound, "_10$") ~ "color",
-      str_detect(sound, "_11$") ~ "read",
-      str_detect(sound, "_12$") ~ "play",
-      str_detect(sound, "_13$") ~ "count"
-    ),
-    order = str_extract(sound, "(o[1-4])"),
-    sound = case_when(
-      str_detect(sound, "babble") ~ "babble",
-      str_detect(sound, "music") ~ "music",
-      str_detect(sound, "silence") ~ "silence",
-      str_detect(sound, "construction") ~ "construction")) |> 
-  pivot_longer(cols = child_age:children_ages_3_4,
-               names_pattern = "(.*)",
-               values_to = "age") |> 
-  pivot_longer(cols = c(sex, sex_multiple_number_1_1,sex_multiple_number_1_2, sex_multiple_number_1_3),
-               names_pattern = "(.*)",
-               values_to = "sex") |> 
-  pivot_longer(cols = c(gender, gender_multiple_number_1_1,gender_multiple_number_1_2,gender_multiple_number_1_3),
-               names_pattern = "(.*)",
-               values_to = "gender") |> 
-  pivot_longer(cols = c(hispanic, hispanic_multiple_number_1_1,hispanic_multiple_number_1_2,hispanic_multiple_number_1_3),
-               names_pattern = "(.*)",
-               values_to = "hispanic") |> 
-  pivot_longer(cols = c(race, race_multiple_number_1_1,race_multiple_number_1_2,race_multiple_number_1_3),
-               names_pattern = "(.*)",
-               values_to = "race") |> 
-  pivot_longer(cols = c(hearing, hearing_multiple_number_1_1,hearing_multiple_number_1_2,hearing_multiple_number_1_3),
-               names_pattern = "(.*)",
-               values_to = "hearing") |> 
-  pivot_longer(cols = c(device_238, device_multiple_number_1_1,device_multiple_number_1_2,device_multiple_number_1_3),
-               names_pattern = "(.*)",
-               values_to = "device") |> 
-  filter(
-    !is.na(rating),
-    !is.na(gender),
-    !is.na(age),
-    !is.na(sex),
-    !is.na(hispanic),
-    !is.na(race),
-    !is.na(hearing),
-    !is.na(device)
-  ) |> 
-  select(-c(number_sex, number_gender, number_children, number_hispanic, number_race, number_hearing, number_device)) |> 
-  select(age, sex, gender, everything())
+  mutate(activity = str_extract(sound, "\\d+$"),
+         sound = str_replace(sound, "_\\d+$", ""),
+         activity = case_when(
+           activity == 1 ~ "listen_story",
+           activity == 2 ~ "sleep",
+           activity == 3 ~ "learn_game",
+           activity == 4 ~ "dance",
+           activity == 5 ~ "math",
+           activity == 6 ~ "talk_about_day",
+           activity == 7 ~ "remember_list",
+           activity == 8 ~ "brush_teeth",
+           activity == 9 ~ "puzzle",
+           activity == 10 ~ "color",
+           activity == 11 ~ "read",
+           activity == 12 ~ "play",
+           activity == 13 ~ "count")) |>
+  filter(!is.na(rating))
 
-View(metanoise_p1_tidy)
+#Join demographic and trial dfs by response_id and set
+metanoise_p2_tidy <- full_join(x = metanoise_p2_trials,
+                               y = metanoise_p2_demographics,
+                               by = c("response_id", "set")) |>
+  arrange(response_id) |> 
+  group_by(response_id) |> 
+  mutate(subject_id = cur_group_id()) |>
+  select(subject_id, everything()) |>
+  ungroup() |>
+  group_by(activity, sound) |> 
+  mutate(rating = case_when(rating == "Very hard" ~ 1,
+                            rating == "A little hard" ~ 2,
+                            rating == "Neither hard nor easy" ~ 3,
+                            rating == "A little easy" ~ 4,
+                            rating == "Very easy" ~ 5),
+         mean_rating = mean(rating),
+         sd_rating = sd(rating))
+
+#Analyze other survey question trends 
+metanoise_p2_other_qs <- metanoise_p2_tidy |>
+  group_by(subject_id) |> 
+  mutate(chaos_commotion = if_else(chaos_commotion == FALSE, 1, 0),
+         chaos_zoo = if_else(chaos_zoo == TRUE, 1, 0),
+         chaos_talk = if_else(chaos_talk == FALSE, 1, 0),
+         chaos_fuss = if_else(chaos_fuss == TRUE, 1, 0),
+         chaos_think = if_else(chaos_think == TRUE, 1, 0),
+         chaos_relax = if_else(chaos_relax == FALSE, 1, 0),
+         chaos_calm = if_else(chaos_calm == FALSE, 1, 0), #Score CHAOS- items 1,2,4,7,12,14,15 = FALSE = items 3,5,6,8,9,10,11,13 = TRUE = 1
+         chaos_score = chaos_commotion + chaos_zoo + chaos_talk + chaos_fuss + chaos_think + chaos_relax + chaos_calm,
+         door_fit = case_when(door_fit == "Not sure" ~ 0,
+                              door_fit == "Serious problem" ~ 3,
+                              door_fit == "Minor problem" ~ 2,
+                              door_fit == "Not a problem" ~ 1),
+         noise_in_home = case_when(noise_in_home == "Not sure" ~ 0,
+                                   noise_in_home == "Serious problem" ~ 3,
+                                   noise_in_home == "Minor problem" ~ 2,
+                                   noise_in_home == "Not a problem" ~ 1),
+         noise_other_homes = case_when(noise_other_homes == "Not sure" ~ 0,
+                                       noise_other_homes == "Serious problem" ~ 3,
+                                       noise_other_homes == "Minor problem" ~ 2,
+                                       noise_other_homes == "Not a problem" ~ 1),
+         noise_street = case_when(noise_street == "Not sure" ~ 0,
+                                            noise_street == "Serious problem" ~ 1,
+                                            noise_street == "Minor problem" ~ 2,
+                                            noise_street == "Not a problem" ~ 3),
+         trash = case_when(trash == "Not sure" ~ 0,
+                           trash == "Serious problem" ~ 3,
+                           trash == "Minor problem" ~ 2,
+                           trash == "Not a problem" ~ 1),
+         traffic = case_when(traffic == "Very heavy" ~ 4,
+                             traffic == "Quite heavy" ~ 3,
+                             traffic == "Not very heavy" ~ 2,
+                             traffic == "Hardly any traffic" ~ 1),
+         shops_walk = case_when(shops_walk == "Strongly agree" ~ 1,
+                                shops_walk == "Somewhat agree" ~ 2,
+                                shops_walk == "Neither agree nor disagree" ~ 3,
+                                shops_walk == "Somewhat disagree" ~ 4,
+                                shops_walk == "Strongly disagree" ~ 5),
+         transit_walk = case_when(transit_walk == "Strongly agree" ~ 1,
+                                transit_walk == "Somewhat agree" ~ 2,
+                                transit_walk == "Neither agree nor disagree" ~ 3,
+                                transit_walk == "Somewhat disagree" ~ 4,
+                                transit_walk == "Strongly disagree" ~ 5),
+         traffic_walk = case_when(traffic_walk == "Strongly agree" ~ 5,
+                                  traffic_walk == "Somewhat agree" ~ 4,
+                                  traffic_walk == "Neither agree nor disagree" ~ 3,
+                                  traffic_walk == "Somewhat disagree" ~ 2,
+                                  traffic_walk == "Strongly disagree" ~ 1),
+         traffic_bike = case_when(traffic_bike == "Strongly agree" ~ 5,
+                                  traffic_bike == "Somewhat agree" ~ 4,
+                                  traffic_bike == "Neither agree nor disagree" ~ 3,
+                                  traffic_bike == "Somewhat disagree" ~ 2,
+                                  traffic_bike == "Strongly disagree" ~ 1)) |> 
+  group_by(subject_id, activity, sound)
+
+#Data Visualization
+##Performance rating by activity and sound (bar)
+ggplot(metanoise_p2_tidy, mapping = aes(x = activity, y = mean_rating, fill = sound)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  geom_errorbar(aes(y = mean_rating,
+                    ymin = mean_rating - sd_rating,
+                    ymax = mean_rating + sd_rating),
+                position = "dodge",
+                width = 0.2) + 
+  facet_wrap(~sound) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+##Performance rating by activity and sound (scatter)
+ggplot(metanoise_p2_tidy, mapping = aes(x = activity, y = rating, col = sound)) +
+  geom_point(stat = "identity", alpha = 0.5, position = position_jitter(width = 0.2)) +
+  geom_errorbar(aes(y = mean_rating,
+                    ymin = mean_rating - sd_rating,
+                    ymax = mean_rating + sd_rating),
+                position = position_dodge(width = 0.2),
+                width = 0.2) + 
+  facet_wrap(~sound) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+##TV usage by age 
+ggplot(metanoise_p2_demographics, mapping = aes(x = age, y= tv, fill = age)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_errorbar(aes(y = mean_tv,
+                    ymin = mean_tv - sd_tv,
+                    ymax = mean_tv + sd_tv),
+                position = "dodge",
+                width = 0.2)
+
+##Performance rating by activity, sound, and age (bar)
+ggplot(metanoise_p2_tidy, mapping = aes(x = age, y = mean_rating, fill = sound)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  geom_errorbar(aes(y = mean_rating,
+                    ymin = mean_rating - sd_rating,
+                    ymax = mean_rating + sd_rating),
+                stat = "identity",
+                position = position_dodge(width = 0.9),
+                width = 0.2) + 
+  facet_wrap(~activity)
+
+##Performance rating by activity, sound, and age (geom_smooth)
+ggplot(metanoise_p2_tidy, mapping = aes(x = age, y = rating, col = sound)) +
+  geom_point(position = position_dodge(width = 0.2), stat = "identity") +
+  geom_smooth(method = "lm") + 
+  facet_wrap(~activity)
+
+Ωz#Performance by CHAOS score
+ggplot(metanoise_p2_other_qs, mapping = aes(x = rating, y = chaos_score, col = sound)) +
+  geom_point(position = position_dodge(width = 0.2), stat = "identity") +
+  geom_smooth(method = "lm") + 
+  facet_wrap(~activity)
+
+
+##Model performance
+metanoise_glmer <- stan_glmer(rating ~ activity * sound + (1|subject_id),
+                              data = metanoise_p2_tidy,
+                              family = "gaussian") 
+summary(metanoise_glmer)
+
+rating ~ activity * sound * age (1|subject_id)
 
